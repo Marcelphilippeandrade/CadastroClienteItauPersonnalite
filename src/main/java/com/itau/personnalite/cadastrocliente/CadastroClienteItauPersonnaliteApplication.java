a312b47b2dad88755583893b33880692158e56be
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2023-05-13 20:23:39 -0300

    Configuracoes iniciais do projeto.

---
+++ src/main/java/com/itau/personnalite/cadastrocliente/CadastroClienteItauPersonnaliteApplication.java
@@ -0,0 +1,13 @@
+package com.itau.personnalite.cadastrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroClienteItauPersonnaliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClienteItauPersonnaliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/personnalite/cadastrocliente/CadastroClienteItauPersonnaliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.personnalite.cadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteItauPersonnaliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
