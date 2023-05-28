a5f3717aa4f3ba46225a472edda3ca8cf2186232
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2023-05-28 14:15:00 -0300

    Criando as validacoes de nome e idade.

--- src/main/java/com/itau/personnalite/cadastrocliente/validation/ValidadorNome.java
+++ src/main/java/com/itau/personnalite/cadastrocliente/validation/ValidadorNome.java
@@ -1,28 +1,15 @@
 package com.itau.personnalite.cadastrocliente.validation;
 
 import java.lang.reflect.Field;
-
-import com.itau.personnalite.cadastro.annotation.IdadeMinima;
 import com.itau.personnalite.cadastro.annotation.Nome;
-import com.itau.personnalite.cadastrocliente.dtos.UsuarioDto;
 
 public class ValidadorNome {
-
-	private static final String REGEX_NOME = "^(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+(?:\\-(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+)*(?: (?:(?:e|y|de(?:(?: la| las| lo| los))?|do|dos|da|das|del|van|von|bin|le) )?(?:(?:(?:d'|D'|O'|Mc|Mac|al\\-))?(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+|(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+(?:\\-(?:[\\p{Lu}&&[\\p{IsLatin}]])(?:(?:')?(?:[\\p{Ll}&&[\\p{IsLatin}]]))+)*))+(?: (?:Jr\\.|II|III|IV))?$";
 
 	public static <T> boolean validador(T objeto) {
 		Class<?> classe = objeto.getClass();
 		for (Field field : classe.getDeclaredFields()) {
 			if (field.isAnnotationPresent(Nome.class)) {
 				Nome nomeComRegexValido = field.getAnnotation(Nome.class);
-
-//				if (classe.getClass().isInstance(UsuarioDto.class)) {
-//					UsuarioDto usuarioDto = (UsuarioDto) objeto;
-//
-//					if (usuarioDto.getNome().matches(REGEX_NOME)) {
-//						return true;
-//					}
-//				}
 				try {
 					field.setAccessible(true);
 					String nome = (String) field.get(objeto);
